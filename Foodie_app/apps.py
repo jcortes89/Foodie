8c7047d1de00cdbf95acd3fcdaa770b77870ee3a
Jose Cortes <jcortes8989@gmail.com>
2022-12-07 12:00:15 -0800

    Commiting more

--- Foodie_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FoodieAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Foodie_app'
--- Foodie_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Foodie_app' # for namespacing
-urlpatterns = [
-    path('/', views.index, name='index')
-]
